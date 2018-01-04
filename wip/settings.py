55a2a8545def475dd4189f758917ffda9ef0af5f
peter soleeh <petersoleeh@gmail.com>
2018-01-04 12:30:24 +0300

    deploy

--- wip/settings.py
+++ wip/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jana!6p23s_0%%s4_z6!rm74c_y!em(d)6*_dl#7&)9%8-8pjh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
